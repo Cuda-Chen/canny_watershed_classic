58b6314ae2baa9ce206003c4e2e56c1a8b37a7d0
Cuda Chen <clh960524@gmail.com>
2018-08-01 00:01:53 +0800

    [milestone] finish the runnable first version of this program
    Finish the program containing the following works:
    1. add file "canny_watershed.py" as main code
    2. add two images "coins.jpg" and "四破魚(藍圓鰺)2.jpg" for testing
    3. create conda environment named "canny_watershed_classic" and
    export the environment settings to "environment.yml"
    4. modify "README.md" to let other people the development tools
    I use

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # canny_watershed_classic
 an image segmentation practice using canny edge detection and watershed algorithm
+
+# Note
+I use Anaconda to develop this project, and the requirements are written in ```environment.yml```
---
+++ canny_watershed.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+import cv2 as cv
+import numpy as np
+
+# first, read the image
+#image = cv.imread('coins.jpg')
+image = cv.imread('四破魚(藍圓鰺)2.jpg')
+cv.imshow('Original image', image)
+
+'''
+part of misc
+'''
+# change image from BGR to grayscale
+gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+# apply thresholding to convert the image to binary
+ret, thresh = cv.threshold(gray, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+# erode the image
+foreground = cv.erode(thresh, None, iterations = 1)
+# Dilate the image
+backgroundTemp = cv.dilate(thresh, None, iterations = 1)
+# Apply thresholding
+ret, background = cv.threshold(backgroundTemp, 1, 128, 1)
+# Add foreground and background
+marker = cv.add(foreground, background)
+
+'''
+part of canny
+'''
+# apply (Gaussian) filter for canny edge detector preprocessing
+gaussian = cv.GaussianBlur(marker, (5, 5), 0)
+# apply canny edge detection
+canny = cv.Canny(gaussian, 100, 300)
+
+'''
+part of watershed
+'''
+# Finding the contors in the image using chain approximation
+new, contours, hierarchy = cv.findContours(canny, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+# converting the marker to float 32 bit
+marker32 = np.int32(marker)
+# Apply watershed algorithm
+cv.watershed(image, marker32)
+# Apply thresholding on the image to convert to binary image
+m = cv.convertScaleAbs(marker32)
+ret, thresh = cv.threshold(m, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+# Invert the thresh
+thresh_inv = cv.bitwise_not(thresh)
+# Bitwise and with the image mask thresh
+res = cv.bitwise_and(image, image, mask = thresh)
+# Bitwise and the image with mask as threshold invert
+res3 = cv.bitwise_and(image, image, mask = thresh_inv)
+# Take the weighted average
+res4 = cv.addWeighted(res, 1, res3, 1, 0)
+# Draw the contours on the image with green color and pixel width is 1
+final = cv.drawContours(res4, contours, -1, (0, 255, 0), 1)
+
+# Display the image
+cv.imshow("Watershed", final) 
+# Wait for key stroke
+cv.waitKey()
