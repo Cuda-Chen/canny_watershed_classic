0677b2adadfd9c06124780e8c907eb1fabac0c0a
Cuda-Chen <clh960524@gmail.com>
2018-09-20 14:48:13 +0800

    [change] isolate the segmentation image object
    Isolate the segmentation image object for the ease of typing.

--- superpixel.py
+++ superpixel.py
@@ -11,11 +11,14 @@ image = img_as_float(io.imread('coins.jpg'))
 segments_slic = slic(image, n_segments = 100, sigma = 5)
 segments_quickshift = quickshift(image, kernel_size = 5, max_dist = 6, ratio = 0.5)
 
+slic_result = mark_boundaries(image, segments_slic, color = (1, 0, 0))
+quickshift_result = mark_boundaries(image, segments_quickshift, color = (1, 0, 0))
+
 fig, ax = plt.subplots(1, 2, figsize=(20, 10), sharex=True, sharey=True)
 
-ax[0].imshow(mark_boundaries(image, segments_slic, color = (1, 0, 0)))
+ax[0].imshow(slic_result)
 ax[0].set_title('slic')
-ax[1].imshow(mark_boundaries(image, segments_quickshift, color = (1, 0, 0)))
+ax[1].imshow(quickshift_result)
 ax[1].set_title('quickshift')
 
 for a in ax.ravel():
