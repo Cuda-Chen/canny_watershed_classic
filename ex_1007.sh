3c68695fe372c958fa6be73c26e68f4e24eb9dcc
Cuda Chen <clh960524@gmail.com>
2018-10-08 00:14:26 +0800

    [change] change the content of ex_1007.sh
    Change the content of ex_1007.sh for generating experiment result.

--- ex_1007.sh
+++ ex_1007.sh
@@ -1,6 +1,6 @@
 for file in {四破魚\(藍圓鰺\)2.jpg,七星鱸.jpg,香魚.jpg,DSC_0007.jpg,DSC_0130.jpg,DSC_0188.jpg}
 do
-    echo python segMethod.py $file 5 5 500 50 10;
-    echo python segMethod.py $file 25 25 1000 150 30;
-    echo python segMethod.py $file 45 45 1500 250 50;
+    pythonw segMethod.py $file 5 5 500 50 10;
+    pythonw segMethod.py $file 25 25 1000 150 30;
+    pythonw segMethod.py $file 45 45 1500 250 50;
 done
