35a6ae2a939687354d32312e75d179e31c0b9ee9
Cuda Chen <clh960524@gmail.com>
2018-10-07 16:42:40 +0800

    [add][change] add shell script and change content of segMethod.py
    1. Add shell script ex_1007.sh for experiment.
    2. Change content of segMethod.py for experiment.

--- segMethod.py
+++ segMethod.py
@@ -1,9 +1,11 @@
+# -*- coding: UTF-8 -*-
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 import cv2 as cv
 import random as rng
 import argparse
+import time
 
 from skimage.segmentation import felzenszwalb, slic, quickshift
 from skimage.segmentation import mark_boundaries
@@ -123,18 +125,37 @@ if __name__ == "__main__":
     print(output_slic)
     print(output_quickshift)
 
+    # mean shift
+    tStart = time.time()
     meanshift_result = mean_shift(inputfile, sp=args.meanshift_sp, sr=args.meanshift_sr)
+    tEnd = time.time()
+    print("Mean shift cost %f sec" % (tEnd - tStart))
 
     image = img_as_float(io.imread(inputfile))
 
+    # felzenszwalb
+    tStart = time.time()
     segment_felzenszwalb = felzenszwalb(image, sigma=superpixel_sigma, min_size=args.fz_min_size)
+    tEnd = time.time()
+    print("felzenszwalb_result cost %f sec" % (tEnd - tStart))
+
+    # SLIC
+    tStart = time.time()
     segment_slic = slic(image, sigma=superpixel_sigma, n_segments=args.SLIC_n_segments)
+    tEnd = time.time()
+    print("SLIC cost %f sec" % (tEnd - tStart))
+
+    # quickshift
+    tStart = time.time()
     segment_quickshift = quickshift(image, kernel_size=5, max_dist=args.quick_shift_max_dist, ratio=0.5)
+    tEnd = time.time()
+    print("quickshift cost %f sec" % (tEnd - tStart))
 
     felzenszwalb_result = mark_boundaries(image, segment_felzenszwalb, color=superpixel_color)
     slic_result = mark_boundaries(image, segment_slic, color=superpixel_color)
     quickshift_result = mark_boundaries(image, segment_quickshift, color=superpixel_color)
 
+    '''
     fig, ax = plt.subplots(2, 2, figsize=(20, 10), sharex=True, sharey=True)
 
     ax[0, 0].imshow(meanshift_result)
@@ -151,6 +172,7 @@ if __name__ == "__main__":
 
     plt.tight_layout()
     plt.show()
+    '''
     
     io.imsave(output_meanshift, meanshift_result)
     io.imsave(output_felzenszwalb, felzenszwalb_result)
